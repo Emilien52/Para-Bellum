17034ecbd58de777367f3d6ec76f9dd0dab318fb
Emilien52 <40137638+Emilien52@users.noreply.github.com>
2020-05-06 18:18:23 +0200

    Mise à jour footer

--- pied.php
+++ pied.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8">
-                <p><?php echo $_Serveur_['General']['name']; ?> - Site web généré via <a href="https://craftmywebsite.fr/" target="_blank">CraftMyWebsite<a/> avec le thème <a href="http://oxykis.hive4code.fr" target="_blank">Para Bellum développé par Oxykis</a>, tous droits réservés</p>
+                <p><?php echo $_Serveur_['General']['name']; ?> - Site web généré via <a href="https://craftmywebsite.fr/" target="_blank">CraftMyWebsite<a/> avec le thème Para Bellum par Oxykis, repris par Emilien52 et BadiiiX</p>
             </div>
             <div class="col-md-4 footer-right">
                 <a class="footer-link" href="<?php echo $_Theme_['Footer']['link-icon1']; ?>" target="_blank"><i class="<?php echo $_Theme_['Footer']['icon1']; ?>"></i></a>
