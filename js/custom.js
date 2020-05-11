540bbfd8c7ae90ad6ca8bee08290b7a5e3b2729c
Emilien52 <40137638+Emilien52@users.noreply.github.com>
2020-05-11 23:38:06 +0200

    Fix page vote

--- js/custom.js
+++ js/custom.js
@@ -19,10 +19,26 @@ $(window).on('load', function () { // makes sure the whole site is loaded
 })
 //]]>
 
-$('.js-scroll-trigger').click(function(){
-    $('html, body').animate({scrollTop:750},'50');
-});
-
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+function bouclevote(id2, pseudo2) {
+	$.post("index.php?action=voter", 
+	{
+		id: id2,
+		pseudo: pseudo2
+	},function(data, status){ 
+		console.log(data);
+		data = data.substring(data.indexOf('[DIV]')+5);
+		if(data == "success")
+		{
+			$("#vote-success").fadeIn(500);setTimeout(function(){ $("#vote-success").fadeOut(500);}, 5000);
+			$("#btn-verif-" + id2).fadeOut(500);setTimeout(function(){ $("#btn-after-" + id2).fadeIn(500);}, 500);
+			if(document.getElementById("nbr-vote-"+pseudo2)){
+				document.getElementById("nbr-vote-"+pseudo2).innerHTML= (parseInt(document.getElementById("nbr-vote-"+pseudo2).innerHTML) + 1);
+			} 
+		}
+		else {
+			setTimeout(function(){ 
+				bouclevote(id2, pseudo2);
+			}, 500);
+		}
+    });
+}
